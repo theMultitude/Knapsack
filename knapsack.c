db5fe99eb03ff51d8b6483b619c37f4608dc249a
theMultitude <os.schroeder@gmail.com>
2018-04-19 15:48:32 -0500

    loading data

--- knapsack.c
+++ knapsack.c
@@ -3,6 +3,10 @@
 
 int main()
 {
+
+  int weight[10] = {0}; //TODO: Use malloc there
+  int value[10] = {0};
+
   //from the command line get the file to load
   char *tempFile = "./data/small1.txt"; //TODO load from param
   printf("Theesa file called %s\n", tempFile);
@@ -16,14 +20,37 @@ int main()
   FILE *fptr;
 
   if ((fptr = fopen(tempFile, "r")) == NULL) {
-    printf("Uh oh, error opening file.");
+    printf("Uh oh, error opening file.\n");
 
       exit(1);
   }
+  int count = 0;
+  int index = -1;
 
-  fscan(fptr, %d, &num)
+  while(fscanf(fptr, %d, &num) != 1)
+  {
+    printf("Value of n=%d\n", num);
 
-  printf("Value of n=%d", num);
+    switch (counter % 3)
+    {
+      case 0:
+        index++;
+        break;
+      case 1:
+        weight[index] = num;
+        break;
+      case 2:
+      value[index] = num;
+        break;
+    }
+
+    counter++;
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%d\n", weight[i])
+  }
+
   fclose(fptr);
 
   //set the knapsack size to the number in second parameter
